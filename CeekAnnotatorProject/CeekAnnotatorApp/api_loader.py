271ee9d4a2de499e8bdc529ef8467451a7a14c3e
Edie Farah <edie@kuhcoon.com>
2016-01-16 18:16:46 -0800

    Annotation Attached to loading offers from parse

--- CeekAnnotatorProject/CeekAnnotatorApp/api_loader.py
+++ CeekAnnotatorProject/CeekAnnotatorApp/api_loader.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from models.job_offer import JobOffer
+from models.job_annotation import JobAnnotation
 import json
+import urllib2
 from parse_rest.connection import register
 from parse_rest.datatypes import Object
 
@@ -70,39 +72,68 @@ class ApiLoader(object):
     def retrieve_offers(self):
         offers_list = []
         all_offers = JobOfferParse.Query.all()
-        for offer in all_offers:
-            id = offer.id
-            title = offer.title
-            description = offer.description
-            compnay_url = offer.compnay_url
-            compnay_logo = offer.compnay_logo
-            created_at = offer.created_at
-            company = offer.company
-            how_to_apply = offer.how_to_apply
-            location = offer.location
-            url = offer.url
-            type = offer.type
+        #for offer in all_offers:
+        offer = all_offers[0]
+        print offer
+        id = offer.id
+        title = offer.title
+        description = offer.description
+        compnay_url = offer.compnay_url
+        compnay_logo = offer.compnay_logo
+        created_at = offer.created_at
+        company = offer.company
+        how_to_apply = offer.how_to_apply
+        location = offer.location
+        url = offer.url
+        type = offer.type
 
-            job_offer = JobOffer.create(id,title,description,compnay_url,compnay_logo,created_at,company,how_to_apply,location,url,type)
-            json_job_offer = job_offer.to_JSON()
+        job_offer = JobOffer.create(id,title,description,compnay_url,compnay_logo,created_at,company,how_to_apply,location,url,type)
+        json_job_offer = job_offer.to_JSON()
 
-            row_as_dict = {
-                'id' : id,
-                'title' : title,
-                'description': description,
-                'compnay_url' : compnay_url,
-                'compnay_logo' : compnay_logo,
-                'created_at' : created_at,
-                'company' : company,
-                'how_to_apply' : how_to_apply,
-                'location' : location,
-                'url' : url,
-                'type' : type
-            }
+        annotations_list = self.annotate_offer(description)
 
-            offers_list.append(row_as_dict)
+        print 'annotation attached to Description'
+        print annotations_list
+        if annotations_list:
+            for annotation in annotations_list:
 
-            print offer.title
+                surface_form = annotation["surface_form"]
+                support = annotation["support"]
+                uri = annotation["uri"]
+                percentage_of_second_rank = annotation["percentage_of_second_rank"]
+                types = annotation["types"]
+                offset = annotation["offset"]
+                similarity_score = annotation["similarity_score"]
+
+                annotation_object = JobAnnotation.create(surface_form=surface_form,support=support,uri=uri,percentage_of_second_rank=percentage_of_second_rank,types=types,offset=offset,similarity_score=similarity_score)
+                jobAnnotationParse = JobAnnotationParse(surface_form=surface_form,support=support,uri_link=uri,percentage_of_second_rank=percentage_of_second_rank,types=types,offset=offset,similarity_score=similarity_score)
+
+                annotation_object.to_JSON()
+                #jobAnnotationParse.save()
+                try:
+                    offer.annotation = jobAnnotationParse
+                    offer.save()
+                except Exception, e:
+                    print e.message
+
+
+        row_as_dict = {
+            'id' : id,
+            'title' : title,
+            'description': description,
+            'compnay_url' : compnay_url,
+            'compnay_logo' : compnay_logo,
+            'created_at' : created_at,
+            'company' : company,
+            'how_to_apply' : how_to_apply,
+            'location' : location,
+            'url' : url,
+            'type' : type
+        }
+
+        offers_list.append(row_as_dict)
+
+        print offer.title
         return offers_list
 
     def annotate_offer(self,text):
@@ -112,34 +143,37 @@ class ApiLoader(object):
 
         respond = requests.get(url=dbPediaBaseURL,headers={"Accept":"application/json"})
 
-        data = json.loads(respond.text)
-
         annotations_list = []
+        try:
+            if(respond):
+                data = json.loads(respond.text)
 
-        resources = data['Resources']
+                if data:
+                    resources = data['Resources']
 
-        for resource in resources:
+                    if(resources):
+                        for resource in resources:
+                            support = resource['@support']
+                            uri = resource['@URI']
+                            surface_form = resource['@surfaceForm']
+                            offset = resource['@offset']
+                            percentage_of_second_rank = resource['@percentageOfSecondRank']
+                            similarity_score = resource['@similarityScore']
+                            types = resource['@types']
 
-            support = resource['@support']
-            uri = resource['@URI']
-            surface_form = resource['@surfaceForm']
-            offset = resource['@offset']
-            percentage_of_second_rank = resource['@percentageOfSecondRank']
-            similarity_score = resource['@similarityScore']
-            types = resource['@types']
+                            row_as_dict = {
+                                    'support' : support,
+                                    'uri' : uri,
+                                    'surface_form': surface_form,
+                                    'offset' : offset,
+                                    'percentage_of_second_rank' : percentage_of_second_rank,
+                                    'similarity_score': similarity_score,
+                                    'types': types
+                                }
 
-            row_as_dict = {
-                    'support' : support,
-                    'uri' : uri,
-                    'surface_form': surface_form,
-                    'offset' : offset,
-                    'percentage_of_second_rank' : percentage_of_second_rank,
-                    'similarity_score': similarity_score,
-                    'types': types
-                }
-
-            annotations_list.append(row_as_dict)
-
+                            annotations_list.append(row_as_dict)
+        except Exception, e:
+            print str(e)
         return annotations_list
 
     def filter_offers(self,company,location,position):
@@ -205,4 +239,7 @@ class ApiLoader(object):
 
 
 class JobOfferParse(Object):
+    pass
+
+class JobAnnotationParse(Object):
     pass
